b2fc72f33e156485b0fe9a866a48a4aafaa9defa
joh2258457 <joh2258457@maricopa.edu>
2023-08-31 19:33:26 -0700

    Initial commit

---
+++ src/main/java/com/csc205/Calculator.java
@@ -0,0 +1,29 @@
+package com.csc205;
+
+import java.util.Arrays;
+
+public class Calculator {
+
+    public int add(int a, int... b) {
+        return a + Arrays.stream(b).sum();
+    }
+
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int subtract(int a, int b)
+    {
+        return a - b;
+    }
+
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public int divide(int a, int b) {
+        return a / b;
+    }
+}
+
+
